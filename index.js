3dc7b8c47b015714b3e701bc522e6237d795f309
HariKrishnan H <harikrishnan.h@diagnal.com>
2022-06-27 16:18:26 +0530

    Adding averaging logic.

--- index.js
+++ index.js
@@ -18,21 +18,36 @@ try {
             log('Hello I am starting');
             const startingTime = new Date().valueOf();
             log('Starting time : ', startingTime);
-            let x = 0
+            let x = 1
             for (let i = 1; i < 10000; i++) {
-                x = x*i
+                x = x*i*i
             }
             const endingTime = new Date().valueOf();
             log('Ending time : ', startingTime);
             const timeDifference = endingTime - startingTime;
             setPerfFromLS(timeDifference)
-            window.performanceIndex = timeDifference;
+            // window.performanceIndex = timeDifference;
             log('Time Diff : ', timeDifference);
             return timeDifference;
         }
+        let it1 = 0;
+        let it2 = 0;
+        let it3 = 0;
+        let it4 = 0;
+        let it5 = 0;
+        let it6 = 0;
 
         if(!getPerfFromLS() || noCache) {
-            calculate_Store_Perf()
+            it1 = calculate_Store_Perf();
+            it2 = calculate_Store_Perf();
+            it3 = calculate_Store_Perf();
+            it4 = calculate_Store_Perf();
+            it5 = calculate_Store_Perf();
+            it6 = calculate_Store_Perf();
+
+            window.performanceIndex = (it1 + it2 + it3 + it4 + it5 + it6) / 6
+
+            
         } else if (isDebug) {
             log('Cached performanceIndex is : ', getPerfFromLS())
         }
