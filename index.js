aae06f33e5e07c28786bdfda9b6b44c7ff18755e
HariKrishnan H <harikrishnan.h@diagnal.com>
2022-06-29 07:33:50 +0530

    COde cleanup.

--- index.js
+++ index.js
@@ -14,43 +14,42 @@ try {
             }
         }
 
-        function calculate_Store_Perf() {
-            log('Hello I am starting');
+        function calculate_system_perf() {
+            log('STARTING: js-performance-index');
             function do_a_heavy_task(){ 
                 const startingTime = new Date().valueOf();
-                log('Starting time : ', startingTime);
+                log('js-performance-index: Starting time : ', startingTime);
                 let x = 1
                 for (let i = 1; i < 10000; i++) {
                     x = x*i*i
                 }
                 const endingTime = new Date().valueOf();
                     
-                    log('Ending time : ', startingTime);
+                    log('js-performance-index : Ending time : ', startingTime);
                 return endingTime - startingTime;
             }
             const repArray = [1,2,3,4,5,6];
-            log('repArray', repArray)
             let sum = 0
             repArray.forEach(() => {
                 sum += do_a_heavy_task();
             })
             const avgPerf = sum / 6;
-            log('##avgPerf', avgPerf);
+            log('js-performance-index : avgPerf', avgPerf);
             let index = 'high';
             if (avgPerf > 11) index = 'low';
             else if (avgPerf > 5) index = 'medium' 
             setPerfFromLS(index)
             window.performanceIndex = index;
-            log('Time Diff : ', index);
+            log('js-performance-index : Calculated Index : ', index);
             return index;
         }
 
         if(!getPerfFromLS() || noCache) {
-            calculate_Store_Perf()
+            calculate_system_perf()
         } else if (isDebug) {
             log('Cached performanceIndex is : ', getPerfFromLS())
         }
-        window.computePerformance = calculate_Store_Perf;
+        window.computePerformance = calculate_system_perf;
     })();
 } catch(e) {
     console.error('There is an error while calculating the performance - ', e)
